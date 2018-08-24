c8225a3b1daee77b5a7e438d98a7d2278264cd1d
Jaskaranbir <jaskaranbir.dhillon@gmail.com>
2018-08-24 18:37:14 -0400

    Change GRAFANA_ADDR to REACT_APP_GRAFANA_ADDR

--- hive-frontend/src/config.js
+++ hive-frontend/src/config.js
@@ -1,5 +1,3 @@
-const baseGrafanaServer = process.env.GRAFANA_ADDR || 'http://localhost:3000'
-
 export default {
-  baseGrafanaUri: `${baseGrafanaServer}/d-solo/1azxh5Kmk/bee-hive-monitoring-dashboard?refresh=5s&orgId=1&theme=light`
+  baseGrafanaUri: `${process.env.REACT_APP_GRAFANA_ADDR}/d-solo/1azxh5Kmk/bee-hive-monitoring-dashboard?refresh=5s&orgId=1&theme=light`
 }
